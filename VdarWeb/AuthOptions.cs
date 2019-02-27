ca3404f9513f208159fe1a9cebc734cbe5a8a36d
invektor08 <invektor08@gmail.com>
2019-02-27 09:19:55 +0600

    Изменил key для токена

--- VdarWeb/AuthOptions.cs
+++ VdarWeb/AuthOptions.cs
@@ -12,7 +12,7 @@ namespace VdarWeb
         public const string ISSUER = "VdarApi"; // издатель токена
         public const string AUDIENCE = "http://localhost:50000/"; // потребитель токена
         public const string AUTHORITY = "http://localhost:50001/token"; // владелец токена
-        const string KEY = "vdar.api!my.secret.key.HARD";   // ключ для шифрации
+        const string KEY = "vdar.api!my.secret.key.HARD.$777$";   // ключ для шифрации
         public const int LIFETIME = 5; // время жизни токена - 5 минут
         public static SymmetricSecurityKey GetSymmetricSecurityKey()
         {
